2878b63bcc5e33cb2cee6ed17245d61369c99433
Zhuoran He <zhuo1angT@Outlook.com>
2021-03-31 18:57:13 +0800

    extract hex to u8 functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 pub use base64::encode;
 pub use base64::encode_one_byte;
 pub use base64::encode_two_bytes;
-pub use base64::hex_str_to_u8;
-pub use base64::single_hex_to_u8;
+pub use hex_to_u8::hex_str_to_u8;
+pub use hex_to_u8::single_hex_to_u8;
 
 mod base64;
+mod hex_to_u8;
